1ee2fa10833b0cc8317777be4e9e90cbf6c7d8fe
tamarastrikalkina <tstrikalkina@gmail.com>
2026-09-22 14:28:46 +0200

    start1

--- tests/test_selectors_css.py
+++ tests/test_selectors_css.py
@@ -1,6 +1,20 @@
 import time
+from selenium.webdriver.common.by import By
+
+from conftest import driver
 
 
 class TestSelectorsCSS:
     def test_selectors_css(self, driver):
-        time.sleep(3)
+        time.sleep(2)
+        footer = driver.find_element(By.TAG_NAME, 'footer')
+        print(footer.tag_name)
+
+
+        tools = driver.find_element(By.CSS_SELECTOR, "img[src='/assets/Toolsqa-DZdwt2ul.jpg']")
+        print(tools.get_attribute('src'))
+
+        driver.find_element(By.CSS_SELECTOR,"a[href='/elements']").click()
+        time.sleep(2)
+
+        driver.back()
